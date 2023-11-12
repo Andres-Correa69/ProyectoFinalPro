13c3cc06ed812d99b6e3f9774d7baafd7291b05d
Andres-Correa69 <correavalencia334@gmail.com>
2023-11-12 15:51:33 -0500

    Create Jueces.java

---
+++ poo-torneo-RQ10-03/torneo-deportivo/src/main/java/co/edu/uniquindio/poo/torneodeportivo/Jueces.java
@@ -0,0 +1,55 @@
+package co.edu.uniquindio.poo.torneodeportivo;
+import static co.edu.uniquindio.poo.util.AssertionUtil.ASSERTION;
+
+public class Jueces{
+    
+    //declaracion de variables
+    private String nombre;
+    private String NumLicenciaJuez;
+    private String email;
+    private String celular;
+
+    //constructor
+    public Jueces(String nombre, String numLicenciaJuez, String email, String celular) {
+        ASSERTION.assertion( nombre != null && !nombre.isBlank() , "El nombre completo es requerido");
+        ASSERTION.assertion( numLicenciaJuez != null && !celular.isBlank() , "El celular es requerido");
+        ASSERTION.assertion( email != null && !email.isBlank() , "El email es requerido");
+        ASSERTION.assertion( celular != null && !celular.isBlank() , "El email es requerido");
+
+        this.nombre = nombre;
+        this.NumLicenciaJuez = numLicenciaJuez;
+        this.email = email;
+        this.celular = celular;
+    }
+
+
+    //declaro getter y setters
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNumLicenciaJuez() {
+        return NumLicenciaJuez;
+    }
+    public void setNumLicenciaJuez(String numLicenciaJuez) {
+        NumLicenciaJuez = numLicenciaJuez;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getCelular() {
+        return celular;
+    }
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    
+
+    
+}
